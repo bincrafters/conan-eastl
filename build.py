4334c5cbfc5854f39fcd5b1bc2a080658170fa83
Jerry W <jerrywiltse@gmail.com>
2018-02-22 21:55:42 -0500

    filter shared builds

--- build.py
+++ build.py
@@ -7,5 +7,7 @@ from bincrafters import build_template_default
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder()
+    
+    builder.builds = filter(lambda build: build.options['eastl:shared'] == False , builder.items)
 
     builder.run()
